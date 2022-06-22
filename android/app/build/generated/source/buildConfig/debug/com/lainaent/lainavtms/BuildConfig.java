01701d8186d233865a7c8b227a058cc9f9d29c58
Babarali345 <babar@tecizeverything.com>
2022-06-22 04:34:26 -0700

    first commit

---
+++ android/app/build/generated/source/buildConfig/debug/com/lainaent/lainavtms/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lainaent.lainavtms;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.lainaent.lainavtms";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "9.0.11";
+  // Field from default config.
+  public static final String PUSHER_BEAM_INTANCE_ID = "9cd21a12-80bd-4643-8c18-b353ef56fa0c";
+  // Field from default config.
+  public static final String REACT_APP_SERVER_ENV = "prod";
+}
